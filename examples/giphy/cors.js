9c5084b2ecc895b7a334a5d661e5f630487bc542
simi2817 <sreenivass@TM4112.local>
2023-10-30 15:25:47 +0000

    refactored with async-await

--- examples/giphy/cors.js
+++ examples/giphy/cors.js
@@ -2,17 +2,17 @@ const img = document.querySelector('img');
 const displayGIF = document.getElementById('displayGIF');
 
 const addGIF = () => {
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=rV23E1xCHYRVTeQD5vlXmNKOj6mrgqPL&s=cats', {mode: 'cors'})
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-        img.src = response.data.images.original.url;
-
-    })
-    .catch((err) => {
-        console.log(err);
-    });           
+    const getCats = async() => {
+        try {
+            const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=rV23E1xCHYRVTeQD5vlXmNKOj6mrgqPL&s=cats', {mode: 'cors'});
+            const cats = await response.json();
+            img.src = cats.data.images.original.url;
+        }
+        catch (err) {
+            console.error(err)
+        }
+    }
+    getCats();
 }
 
 let keyword = '';
@@ -22,16 +22,18 @@ search.addEventListener('input', (e) => {
     e.preventDefault();
     keyword = e.target.value;
     });
+
 const searchGIF = () => {
-    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=rV23E1xCHYRVTeQD5vlXmNKOj6mrgqPL&s=${keyword}`, {mode: 'cors'})
-    .then((response) => {
-        return response.json();
-    })
-    .then((response) => {
-        img.src = response.data.images.original.url;
-    })
-    .catch((err) => {
-        img.src = './sad.gif';
-        console.log(err);
-    }); 
+    const getGIF = async() => {
+        try {
+            const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=rV23E1xCHYRVTeQD5vlXmNKOj6mrgqPL&s=${keyword}`, {mode: 'cors'});
+            const gif = await response.json();
+            img.src = gif.data.images.original.url;
+        }
+        catch(err) {
+            img.src = './sad.gif';
+            console.error(err);
+        }
+    }
+    getGIF();
 }
